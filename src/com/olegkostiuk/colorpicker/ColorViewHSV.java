c4a19f5606c821a27323f24a08a0245d32b74f6b
Oleg Kostiuk <olegkostiuk@gmail.com>
2013-08-15 21:00:29 +0300

    Fixed HSV view

--- src/com/olegkostiuk/colorpicker/ColorViewHSV.java
+++ src/com/olegkostiuk/colorpicker/ColorViewHSV.java
@@ -21,9 +21,9 @@ public class ColorViewHSV extends ColorView {
 		Color.RGBtoHSB(RGB[0], RGB[1], RGB[2], HSB);
 	
 		HSB[0]=HSB[0]*360;
-		HSB[1]=HSB[1]*255;
-		HSB[2]=HSB[2]*255;
-		
+		HSB[1]=HSB[1]*100;
+		HSB[2]=HSB[2]*100;
+
 		for (int i = 0; i < RGB.length; i++) {
 			fields[1][i].setText(String.valueOf(Math.round(HSB[i])));
 		}
